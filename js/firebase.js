d306614a608fb8b38a8cfb2dc68246af77b4d240
kurage <hotakudzo@gmail.com>
2026-03-12 19:21:54 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDDB3cswJ9uRH_XiiS_M9BCD1-8M-VStK0",
+    authDomain: "school-portal2-2ebc4.firebaseapp.com",
+    projectId: "school-portal2-2ebc4",
+    storageBucket: "school-portal2-2ebc4.firebasestorage.app",
+    messagingSenderId: "141452998739",
+    appId: "1:141452998739:web:309c7e88b7cf6d80dd2ea6"
+  };
 
-export const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
