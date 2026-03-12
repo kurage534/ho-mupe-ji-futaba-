ac9a91fb6df292013ea220f0d1c7a6aa990864e2
kurage <hotakudzo@gmail.com>
2026-03-12 17:52:16 +0900

    Delete Firebase directory

--- Firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-
-const firebaseConfig = {
- apiKey: "APIKEY",
- authDomain: "PROJECT.firebaseapp.com",
- projectId: "PROJECT",
-};
-
-export const app = initializeApp(firebaseConfig);
