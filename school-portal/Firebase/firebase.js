fbbfc1ec5c86c3c6c05d6e22ad159b1c142c849e
kurage <hotakudzo@gmail.com>
2026-03-12 18:05:15 +0900

    Update firebase.js

--- school-portal/Firebase/firebase.js
+++ school-portal/Firebase/firebase.js
@@ -1,12 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 
 export const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+  apiKey: "AIzaSyD0F4kHjZQuZ_rmSPyHI8VKdSPM0fr78EY",
+  authDomain: "school-portal-9f545.firebaseapp.com",
+  projectId: "school-portal-9f545",
+  storageBucket: "school-portal-9f545.firebasestorage.app",
+  messagingSenderId: "779802463665",
+  appId: "1:779802463665:web:9e5d63a165912dac590190"
 };
 
 export const app = initializeApp(firebaseConfig);
