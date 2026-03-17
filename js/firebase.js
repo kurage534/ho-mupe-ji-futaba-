6a0cb9baa6e46c31eb30bf0b126e5a9d3fb7fdaa
kurage <hotakudzo@gmail.com>
2026-03-17 13:15:42 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,15 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDDB3cswJ9uRH_XiiS_M9BCD1-8M-VStK0",
-    authDomain: "school-portal2-2ebc4.firebaseapp.com",
-    projectId: "school-portal2-2ebc4",
-    storageBucket: "school-portal2-2ebc4.firebasestorage.app",
-    messagingSenderId: "141452998739",
-    appId: "1:141452998739:web:309c7e88b7cf6d80dd2ea6"
-  };
+// Firebase設定（Firebaseコンソールからコピー）
+const firebaseConfig = {
+  apiKey: "AIzaSyDDB3cswJ9uRH_XiiS_M9BCD1-8M-VStK0",
+  authDomain: "school-portal2-2ebc4.firebaseapp.com",
+  projectId: "school-portal2-2ebc4",
+  storageBucket: "school-portal2-2ebc4.firebasestorage.app",
+  messagingSenderId: "141452998739",
+  appId: "1:141452998739:web:309c7e88b7cf6d80dd2ea6"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Firebase初期化
+export const app = initializeApp(firebaseConfig);
